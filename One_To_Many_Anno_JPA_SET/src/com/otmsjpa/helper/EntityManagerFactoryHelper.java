c904ab9c46274b0c713983437082cebb54c2b886
rohit <rohit@DESKTOP-F16QUPD>
2020-07-06 15:00:53 +0530

    Entity Manager factory commit

---
+++ One_To_Many_Anno_JPA_SET/src/com/otmsjpa/helper/EntityManagerFactoryHelper.java
@@ -0,0 +1,38 @@
+ package com.otmsjpa.helper;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerFactoryHelper
+{
+	private static EntityManagerFactory entityManagerFactory;
+	static
+	{
+		try 
+		{
+			entityManagerFactory=Persistence.createEntityManagerFactory("hibpu");
+		}
+		catch (Throwable t)
+		{
+			t.printStackTrace();
+			throw t;
+		}
+	}
+	
+	
+	public static EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
+
+
+	public static void closeEntityManagerFactory()
+	{
+		if (entityManagerFactory != null)
+		{
+			entityManagerFactory.close();
+			entityManagerFactory=null;
+		}
+	}
+	
+}
