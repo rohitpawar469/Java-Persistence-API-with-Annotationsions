e3c4f9a2c3c0334497a9e1cb2b2ef7ed1c50e653
rohit <rohit@DESKTOP-F16QUPD>
2020-07-06 12:27:45 +0530

    added Entity Manager Factory class

---
+++ One_To_One_Direct_Anno_JPA/src/com/otojpa/helper/EntityManagerFactoryHelper.java
@@ -0,0 +1,34 @@
+package com.otojpa.helper;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerFactoryHelper {
+	private static EntityManagerFactory entityManagerFactory;
+
+	static
+	{
+		try 
+		{
+			entityManagerFactory=Persistence.createEntityManagerFactory("hibpu");
+		} catch (Throwable t) {
+			t.printStackTrace();
+			throw t;
+
+		}
+
+	}
+
+	public static EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
+
+	public void closeEntityManagerFactory()
+	{
+		if (entityManagerFactory != null) 
+		{
+			entityManagerFactory.close();
+			entityManagerFactory = null;
+		}
+	}
+}
