fff29bd3d0b9c00eb24073c19775951cd2d3cd1e
rohit <rohit@DESKTOP-F16QUPD>
2020-07-10 09:42:46 +0530

    Entity Manager Factory Helper Class Final Done

--- One_To-Many_Anno_JPA_List/src/com/otmljpa/helper/EntityManagerFactoryHelper.java
+++ One_To-Many_Anno_JPA_List/src/com/otmljpa/helper/EntityManagerFactoryHelper.java
@@ -1,5 +1,35 @@
 package com.otmljpa.helper;
 
-public class EntityManagerFactoryHelper {
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+public class EntityManagerFactoryHelper
+{
+	public static EntityManagerFactory entityManagerFactory;
+	
+	static 
+	{
+		try 
+		{
+			entityManagerFactory=Persistence.createEntityManagerFactory("hibpu");
+		}
+		catch (Throwable t)
+		{
+			t.printStackTrace();
+			throw t;
+		}		
+	}
+
+	public static EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
+	
+	public static void closeEntityManagerFactory()
+	{
+		if (entityManagerFactory != null)
+		{
+			entityManagerFactory.close();
+			entityManagerFactory=null;
+		}
+	}
 }
