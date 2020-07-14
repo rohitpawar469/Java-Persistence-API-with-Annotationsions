30e07238f118e0fe80615893e86e018409c104f1
rohit <rohit@DESKTOP-F16QUPD>
2020-07-14 18:09:35 +0530

    Commit using Typed Querry Class of JPQL API

--- One_To_Many_Anno_JPA_SET/src/com/otmsjpa/test/BasicJPQLTest.java
+++ One_To_Many_Anno_JPA_SET/src/com/otmsjpa/test/BasicJPQLTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import com.otmsjpa.entities.Account;
 import com.otmsjpa.helper.EntityManagerFactoryHelper;
@@ -20,13 +21,19 @@ public class BasicJPQLTest {
 			entityManagerFactory=EntityManagerFactoryHelper.getEntityManagerFactory();
 			entityManager=entityManagerFactory.createEntityManager();
 			
-			Query query=entityManager.createQuery("from Account");
-			List<Account> accounts=query.getResultList();
-			 for (Account account : accounts)
-			 {
-				 System.out.println("Account Holder Name :"+account.getAccountHolderName());
-			 }
+			/*
+			 * Query query=entityManager.createQuery("from Account"); List<Account>
+			 * accounts=query.getResultList(); for (Account account : accounts) {
+			 * System.out.println("Account Holder Name :"+account.getAccountHolderName()); }
+			 * 
+			 */
 			
+			TypedQuery<Account> typedQuery=entityManager.createQuery("from Account",Account.class);
+			List<Account> accounts=typedQuery.getResultList();
+			for (Account account : accounts)
+			{
+				System.out.println("Account Type :"+account.getAccountType());
+			}
 		}
 		finally
 		{
